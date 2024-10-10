f77c652343cf28e4c6607884eaffa8cc8050f02a
SangNguyen <nngocsang38@gmail.com>
2024-10-10 16:37:17 +0700

    fix: move auth server

--- auth-server/src/main/java/org/example/authserver/interfaces/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.authserver.interfaces;
-
-import org.example.authserver.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
